85ceb0aa8c905fee2b52b6e930a9f8a67bc89f88
Jessie Adkins <zaheelee@gmail.com>
2016-11-09 12:24:31 -0500

    made the algorithm for ChatClient

--- ChatClient.java
+++ ChatClient.java
@@ -5,6 +5,52 @@ class ChatClient
 {
 	public static void main(String argv[]) throws Exception
 	{
+		//Put any necessary variables here
+		boolean connectedToServer = false;
 		
+		
+		//TODO create input stream from the keyboard
+		BufferedReader inFromKeyboard = new BufferedReader(new InputStreamReader(System.in));
+		
+		//TODO create client socket and connect to server and port 5001
+		
+		//TODO create output stream attached to socket 
+		
+		//TODO create input stream attached to socket
+		
+		while(connectedToServer) //TODO while connection with server is still valid
+		{
+			//TODO save data taken from keyboard input
+			
+			//TODO see if data is a quit command, set connectedToServer accordingly
+			
+			//TODO send data to server
+			
+			//TODO receive data from server
+			
+			//TODO process data taken from server
+				//TODO if a connection with another user is successful, 
+					//data will also contain whether you speak first
+			
+			boolean myTurnToTalk = false;//set this based on response from server
+			
+			while(true) //TODO while connection with another user is valid
+			{
+				if(myTurnToTalk) //TODO it's your turn to talk
+				{
+					//TODO save data from keyboard input
+					
+					//TODO send data to server
+				}
+				else//TODO it is not your turn to talk
+				{
+					//TODO save data from server
+					
+					//TODO display data from server
+				}
+				myTurnToTalk = !myTurnToTalk;
+			}
+		}
+		//TODO close client socket
 	}
 }
